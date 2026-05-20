9ecf115ea975aedb83fcadd217c9803fb8e19450
LeilaF007 <leilafenicheelfenich@gmail.com>
2026-05-20 13:10:44 +0200

    Update affichage.c

--- affichage.c
+++ affichage.c
@@ -4,13 +4,17 @@
 
 void generer_lignes_carte(int carte, char lignes[5][20]){
     const char *couleur;
-    if(carte >= 13){
+
+    if (carte >= 13) {
         couleur = YELLOW;
     } 
-    else{
+    else {
         couleur = BLUE;
     }
+
+
     sprintf(lignes[0], "%s--------%s", couleur, COLOR_RESET);
+
     switch(carte){ 
         case 13:
             sprintf(lignes[1], "%s|  x2  |%s", couleur, COLOR_RESET);
@@ -30,6 +34,7 @@ void generer_lignes_carte(int carte, char lignes[5][20]){
         case 18: 
             sprintf(lignes[1], "%s| +10  |%s", couleur, COLOR_RESET);
             break;
+    
         default:
             if(carte > 9){
                 sprintf(lignes[1], "%s|  %d  |%s",couleur, carte, COLOR_RESET);
@@ -38,6 +43,7 @@ void generer_lignes_carte(int carte, char lignes[5][20]){
                 sprintf(lignes[1], "%s|   %d  |%s",couleur, carte, COLOR_RESET);
             }
     }
+
     sprintf(lignes[2], "%s--------%s", couleur, COLOR_RESET);
     
     // lignes 3 et 4 vides pour compatibilité
@@ -49,28 +55,35 @@ void generer_lignes_carte(int carte, char lignes[5][20]){
 
 void afficher_plateau(Joueur *j, Pioche *p){
     printf("\n");
-    printf("%s--------------------------------- PLATEAU ----------------------------------%s\n", CYAN, COLOR_RESET);
+    printf("%s╭──────────────────────────────── PLATEAU ─────────────────────────────────╮%s\n", CYAN, COLOR_RESET);
     printf("  JOUEUR : %s%s%s | SCORE TOTAL : %s%d%s | SCORE MANCHE : %s%d%s\n", 
     YELLOW, j->nom, COLOR_RESET, GREEN, j->scoreTotal, COLOR_RESET, RED, j->scoreManche, COLOR_RESET);
     printf("  VOTRE MAIN : \n");
+    
     int cartes[50];
     int nb=0;
-    for(int i = 0; i<= 18; i++){  //recovering present cards
+
+    //recovering present cards
+    for(int i = 0; i<= 18; i++){
         if(j->cartesMain[i]>0){
             cartes[nb++]=i;
         }
     }
+
+    //player has no cards
     if(nb==0){
-        printf("  (vide)\n");  //player has no cards
+        printf("  (vide)\n");
     }
     else{
         char lignes[50][5][20]; // 50 cartes max, 5 lignes, 20 chars
+
         // Génération
-        for(int k = 0; k<nb; k++){
+        for (int k = 0; k < nb; k++) {
             generer_lignes_carte(cartes[k], lignes[k]);
         }
+
         // Affichage ligne par ligne
-        for(int ligne = 0; ligne < 3; ligne++){
+        for (int ligne = 0; ligne < 3; ligne++) {
             for (int k = 0; k < nb; k++) {
                 printf("%s   ", lignes[k][ligne]);
             }
@@ -82,15 +95,15 @@ void afficher_plateau(Joueur *j, Pioche *p){
 
     //progression bar
     printf("  PIOCHE : [");
-    int progression = (p->prochain_indice * 40)/p->taille; //echelle pour la pioche
+    int progression = (p->prochain_indice * 40)/TAILLE_PIOCHE; //echelle pour la pioche
     for(int i=0; i<40; i++){
         if(i<progression){
             printf("X");}
         else{
             printf(".");}
     }
-    printf("] (%d/%d cartes tirées)\n", p->prochain_indice, p->taille);
-    printf("%s----------------------------------------------------------------------------%s\n", CYAN, COLOR_RESET);
+    printf("] (%d/%d cartes tirées)\n", p->prochain_indice, TAILLE_PIOCHE);
+    printf("%s╰──────────────────────────────────────────────────────────────────────────╯%s\n", CYAN, COLOR_RESET);
 }
 
 void attendre_entree(){
