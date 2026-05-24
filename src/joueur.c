fb23653c33e4d46ff4958cc4b2c4929837679aeb
FlorentinSS <florentin.staron@etu.cyu.fr>
2026-05-24 14:21:44 +0200

    Update joueur.c

--- src/joueur.c
+++ src/joueur.c
@@ -5,23 +5,30 @@ void init_joueur_manche(Joueur *j){
     j->scoreManche = 0;
     j->perduManche = 0;
     j->finiManche = 0;
-    for(int i=0; i<19; i++){ //il y a 19 cartes différentes dans le jeu
+
+    //There are 19 different cards in the game
+    for(int i=0; i<19; i++){
         j->cartesMain[i] = 0;
     }
 }
 
 Joueur* connexion(int *nbj){
-    int n = 0; //initialisation des nombres de joueurs
+
+    //initialization of the number of players
+    int n = 0; 
     printf("--- BIENVENUE DANS FLIPTEK ---\n");
     do{
         printf("Combien de joueurs participent ? (Minimum 2) : ");
-        if(scanf("%d", &n) != 1){ //si la fonction scanf ne renvoie pas 1 alors il y a une erreur sur le champ entrer par l'utlisateur
+        if(scanf("%d", &n) != 1){ //If the scanf function does not return 1, then there is an error in the field entered by the user
             printf("Erreur : veuillez saisir un nombre entier\n");
-            while(getchar() != '\n'){}; //le buffer contient autre chose que un entier donc le while permet d'enlever ce qui y contient car c'est faux
-        }
+            while(getchar() != '\n'){}; //The buffer contains something other than an integer, so the while loop allows us to remove what it contains because it's false
     }while(n<2);
-    (*nbj) = n; //le nbr de joueurs
-    Joueur *tab = malloc(n*sizeof(Joueur)); //tableau de joueurs
+
+    //the number of players
+    (*nbj) = n; 
+
+    //players' table
+    Joueur *tab = malloc(n*sizeof(Joueur));
     if(tab == NULL){
         printf("Erreur d'allocation des joueurs\n");
         exit(1);
@@ -30,9 +37,9 @@ Joueur* connexion(int *nbj){
         printf("\n");
         printf("Joueur %d, saisissez votre pseudo : ", i+1);
         scanf("%s", tab[i].nom);
-        while(getchar() != '\n'); //pour enlever le \n present dans le buffer parce que le scanf recupère que le nom
+        while(getchar() != '\n'); //to remove the \n present in the buffer because scanf only retrieves the name
         tab[i].id = i+1;
-        tab[i].scoreTotal =0; //les scores à 0 en début de partie
+        tab[i].scoreTotal =0; //scores at 0 at the start of the game
         init_joueur_manche(&tab[i]);
     }
     printf("\n");
